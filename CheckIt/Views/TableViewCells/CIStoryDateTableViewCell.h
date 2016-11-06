c7ce7a99bffefc8d0e37d4689c4302344ec65fb0
Pablo Marti <startpablo@gmail.com>
2016-11-06 15:28:03 -0600

    Build cell for date

---
+++ CheckIt/Views/TableViewCells/CIStoryDateTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  CIStoryDateTableViewCell.h
+//  CheckIt
+//
+//  Created by Devserker on 11/6/16.
+//  Copyright © 2016 Indigitous. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CIStoryDateTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *lblDate;
+
+@end
---
+++ CheckIt/Views/TableViewCells/CIStoryDateTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CIStoryDateTableViewCell.m
+//  CheckIt
+//
+//  Created by Devserker on 11/6/16.
+//  Copyright © 2016 Indigitous. All rights reserved.
+//
+
+#import "CIStoryDateTableViewCell.h"
+
+@implementation CIStoryDateTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
